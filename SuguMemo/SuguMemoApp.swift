2070c566610f788bd71be173eb280a25d826f13a
nizi <failaforcontact@gmail.com>
2020-11-07 20:24:06 +0900

    Initial Commit

---
+++ SuguMemo/SuguMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuguMemoApp.swift
+//  SuguMemo
+//
+//  Created by Yuu Nishida on 2020/11/07.
+//
+
+import SwiftUI
+
+@main
+struct SuguMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
