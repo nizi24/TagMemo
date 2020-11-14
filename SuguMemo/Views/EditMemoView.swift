b5f844abd0ebbdf077fe319ee9fd1b854d6ca794
nizi <failaforcontact@gmail.com>
2020-11-14 12:33:37 +0900

    [fix] EditMemoViewを整理

--- SuguMemo/Views/EditMemoView.swift
+++ SuguMemo/Views/EditMemoView.swift
@@ -10,32 +10,10 @@ import SwiftUI
 struct EditMemoVIew: View {
     @ObservedObject var viewModel: EditMemoViewModel
     @EnvironmentObject var memoListViewModel: MemoListViewModel
-    var updateMemoList: (MemoListViewModel, EditMemoViewModel) -> Void = { memoListViewModel, viewModel in
-        let memoList = memoListViewModel.memoList
-        memoListViewModel.memoList = memoList.map { memo in
-            if memo.id == viewModel.id {
-                var updateMemo = memo
-                updateMemo.title = viewModel.title
-                updateMemo.context = viewModel.context
-                updateMemo.tags = viewModel.tags
-                return updateMemo
-            }
-            return memo
-        }
-    }
     
     var body: some View {
         VStack {
             MemoForm(title: $viewModel.title, context: $viewModel.context, tags: $viewModel.tags)
-                .onChange(of: viewModel.title, perform: { newTitle in
-//                    updateMemoList(memoListViewModel, viewModel)
-                })
-                .onChange(of: viewModel.context, perform: { newContext in
-//                    updateMemoList(memoListViewModel, viewModel)
-                })
-                .onChange(of: viewModel.tags, perform: { newTags in
-//                    updateMemoList(memoListViewModel, viewModel)
-                })
         }
         .navigationBarTitle(Text(viewModel.title), displayMode: .inline)
     }
